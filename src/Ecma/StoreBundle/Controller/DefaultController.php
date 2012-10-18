bd03ed478b69d3aa105323991debe0192a39e14b
Matei Trusca <matei.trusca@innobyte.com>
2012-10-18 20:26:34 +0300

    generated demo entity and added fixtures bundle

--- src/Ecma/StoreBundle/Controller/DefaultController.php
+++ src/Ecma/StoreBundle/Controller/DefaultController.php
@@ -5,9 +5,11 @@ namespace Ecma\StoreBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Ecma\StoreBundle\Entity\Product;
 
 class DefaultController extends Controller
 {
+
     /**
      * @Route("/hello/{name}")
      * @Template()
@@ -16,4 +18,23 @@ class DefaultController extends Controller
     {
         return array('name' => $name);
     }
+
+    /**
+     * @Route("/create")
+     * @Template("EcmaStoreBundle:Default:index.html.twig")
+     */
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+        $product->setDescription('Lorem ipsum dolor');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+
+        return array('name' => 'Created product id '.$product->getId());
+    }
+
 }
